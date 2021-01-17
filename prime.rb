304768e738786de5fbaf4b80a26215167117e490
KeslieBox <kesliebox@gmail.com>
2021-01-16 18:37:14 -0600

    commit tree 26334fd9d5e15d7bfe0a062137e2bfb9d897cc6a

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
+require 'pry'
 # Add  code here!
+def prime?(n)
+    return false if n < 2
+    return false if !n.integer?
+    return true if n == 2
+    (2..n-1).each {|int| return false if n % int == 0}
+    true
+end
